38619340fda7e45afa148b307aaa6b0af2503bf8
oktoala <okta.yoga.syah@gmail.com>
2021-12-25 13:07:46 +0800

    Able to crate user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -35,14 +37,25 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $validator = $request->validate([
-            'name' => 'required|min:3',
             'username' => 'required|min:3',
+            'name' => 'required|min:3',
             'roles' => 'required',
             'address' => 'min:3',
             'phone' => 'min:3',
             'email' => 'required|email:dns',
-            'password' => 'required|min:3',
+            'password' => 'required|min:3|confirmed',
         ]);
+        $validator['roles'] = json_encode($validator['roles']);
+        $validator['password'] = Hash::make($validator['password']);
+        $validator['avatar'] = "avatars/nopic.png";
+
+        if ($request->hasFile('avatar')){
+            $validator['avatar'] = $request->file('avatar')->store('avatars', 'public');
+        }
+
+        User::create($validator);
+
+        return redirect()->route('users.create')->with('status', "User Berhasil Dibuat");
     }
 
     /**

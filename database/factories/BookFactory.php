919debda457e1d4a01fc5c3a53e6e00defcc43e0
oktoala <okta.yoga.syah@gmail.com>
2021-12-27 11:36:10 +0800

    Add BookFactory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class BookFactory extends Factory
 {
@@ -13,7 +14,19 @@ class BookFactory extends Factory
      */
     public function definition()
     {
+        $status = ['PUBLISH', 'DRAFT'];
+        $title = $this->faker->sentence(3);
+        $slug = Str::slug($title);
         return [
+            'title' => $title,
+            'description' => $this->faker->text(300),
+            'author' => $this->faker->name(),
+            'publisher' => $this->faker->company(),
+            'price' => 100000,
+            'status' => $this->faker->randomElement($status),
+            'slug' => $slug,
+            'created_by' => $this->faker->randomBetween(1, 10),
+            'cover' => 'books-cover/nocover.png',
         ];
     }
 }
--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -58,7 +58,7 @@
                     <li class="{{ Request::is('books*') ? 'active' : '' }}"><a
                             href="{{ route('books.index') }}"><span class="oi oi-book"></span> Manage books</a>
                     </li>
-                    <li class="{{ Request::is('books*') ? 'active' : '' }}"><a
+                    <li class="{{ Request::is('orders*') ? 'active' : '' }}"><a
                             href="{{ route('orders.index') }}"><span class="oi oi-inbox"></span> Manage orders</a>
                     </li>
 
